ba688a2759f4956c3deb4a73890125b441824de9
Joshua Banjo <joshuabanjo.tech@gmail.com>
2024-08-26 20:24:55 +0100

    Maybach Music

---
+++ ages.py
@@ -0,0 +1,6 @@
+user_input_age = int(input("How old are you Sailor?\n"))
+
+if user_input_age < 18:
+    print("Ye be too young to try and tame the cruel mistress of the sea!")
+else:
+    print("Are you sure Sailor? Are you sure?")
