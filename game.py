3178622d2420a455dac7ed27b5b71a57b03a2035
Joshua Banjo <joshuabanjo.tech@gmail.com>
2024-08-26 19:32:47 +0100

    So far so good

--- game.py
+++ game.py
@@ -1,1 +1,3 @@
 print("Escape from Greed Island -___-")
+action_input = input('Action: ')
+print(action_input + " because the only way is up!")
--- test.py
+++ test.py
@@ -1,3 +1,24 @@
 print("Welcome Home")
 greeting = "Hello, World!"
 print(greeting)
+
+name = 'Phillip'
+forecast = 'It\'s going to rain!'
+url = 'http://letstalkdata.com'
+
+arr_test = [name, forecast, url]
+
+for x in arr_test:
+    x += " feeling good about life"
+    print(x)
+
+a = 4
+b = 4
+hypoteneuse = 5
+
+pythagoras = (a**2) + (b**2)
+
+if pythagoras == (hypoteneuse**2):
+    print("So far so good")
+else:
+    print('You\'ve made a mistake somewhere lol')
