70624b4ae3975d8b9630eaafe03f0549c0d64808
Joshua Banjo <joshuabanjo.tech@gmail.com>
2024-08-27 19:12:47 +0100

    Still working

---
+++ lists.py
@@ -0,0 +1,11 @@
+my_list = ['A', 'B', 'C', 'D', 'E']
+my_list.append('F')
+print(my_list)
+print(len(my_list))
+
+print(my_list[0])
+
+last_position = len(my_list) - 1
+
+print(my_list[last_position])
+print(my_list[-1])
