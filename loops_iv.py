52be354141ed1b82d1aa7a747f3e4d163c995d85
Joshua Banjo <joshuabanjo.tech@gmail.com>
2024-08-30 22:19:09 +0100

    Still working

---
+++ loops_iv.py
@@ -0,0 +1,17 @@
+arr_nba_roster = []
+
+more_players = True
+
+while more_players:
+    user_input = input("Who would you like to add to your team?\n")
+
+    if user_input == '':
+        print("Your team is now complete!\n")
+        more_players = False
+    else:
+        arr_nba_roster.append(user_input)
+
+i = 0
+for player in arr_nba_roster:
+    print(f"At the {i + 1} we have {player}")
+    i += 1
